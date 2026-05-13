e1aff4d28e8279b680109e101da2a98b5530ec9f
cirosmzt <ciro.smzt@gmail.com>
2026-05-13 16:29:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,11 @@
 const CACHE = 'pp2026-v29';
-const FILES = [
-  './',
-  './index.html',
-  './banca_dati.json',
-  './manifest.json'
-];
-
+const FILES = ['./', './index.html', './manifest.json', './banca_dati.json'];
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    )).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).then(response => {
-      const clone = response.clone();
-      caches.open(CACHE).then(cache => cache.put(e.request, clone));
-      return response;
-    }))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
